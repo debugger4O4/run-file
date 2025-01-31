6e9c2e2ea38733f9c98fa15fe14aa272d760a599
debugger404 <now-kirill@ya.ru>
2025-01-31 23:00:41 +0300

    Init code and files.

---
+++ run.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	system("javac HelloWorld.java");
+	cout << "Lets's start HelloWorld application!";
+	system("java HelloWorld");
+    return 0;
+}
